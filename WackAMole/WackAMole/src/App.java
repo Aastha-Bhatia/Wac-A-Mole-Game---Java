d45326320c92536bd10e6e27cb0c3de5b6079436
Aastha Bhatia <122430152+Aastha-Bhatia@users.noreply.github.com>
2024-09-09 09:46:10 +0530

    Update App.java

--- WackAMole/WackAMole/src/App.java
+++ WackAMole/WackAMole/src/App.java
@@ -1,3 +1,8 @@
+// Wac/Whack a Mole Game
+// Developed by: Aastha Bhatia
+// Description: A simple Wac-A-Mole game implementation using Java Swing.
+// Feel free to modify and distribute, but please retain this attribution.
+
 public class App {
     public static void main(String[] args) throws Exception {
         WelcomePage WelcomePage=new WelcomePage();
